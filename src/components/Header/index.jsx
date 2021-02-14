0a8671d7be581a21719ae5634ba625dd05a85700
Andre Diop <itsjustandre@gmail.com>
2021-02-14 15:03:18 -0500

    Completed Header instance

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class index extends Component {
+  render () {
+    return (
+      <div>
+        <section className='hero is-link'>
+          <div className='hero-body has-text-centered'>
+            <p className='title is-size-1'>Personnel Tracker</p>
+            <p className='subtitle'>Click on controls to filter by heading or use the search to narrow by results.</p>
+          </div>
+        </section>
+      </div>
+    ) 
+  }
+}
+
+export default index
