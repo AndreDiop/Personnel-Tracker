013f8f6de529541ea0dc4a5c0e966b72632ae32b
Andre Diop <itsjustandre@gmail.com>
2021-02-14 14:41:22 -0500

    Added centered text

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class index extends Component {
-    render() {
-        return (
-            <div>
-               <h2>This is a header</h2> 
-            </div>
-        );
-    }
+  render () {
+    return (
+      <div>
+        <section className='hero is-link'>
+          <div className='hero-body'>
+            <p className='title has-text-centered'>Personnel Tracker</p>
+            <p className='subtitle has-text-centered'>Click on controls to filter by heading or use the search to narrow by results.</p>
+          </div>
+        </section>
+      </div>
+    ) 
+  }
 }
 
-export default index;
+export default index
