925bb560d2d6d6f793ebf5b367715e866366450e
Andre Diop <itsjustandre@gmail.com>
2021-02-14 15:04:14 -0500

    Imported SearchBar
    - added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import Header from "./Header"
-
+import Header from "./components/Header"
+import SearchBar from "./components/SearchBar"
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <SearchBar/>
         <h1>Hello</h1>
       </div>
     );
