a8d11bf9dc905b848405e0263d3fea426e63f3f6
Andre Diop <itsjustandre@gmail.com>
2021-02-14 18:17:25 -0500

    Imported Table component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Header from "./components/Header"
 import SearchBar from "./components/SearchBar"
+import Table from "./components/Table"
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
         <SearchBar/>
-        <h1>Hello</h1>
+        <Table/>
       </div>
     );
   }
