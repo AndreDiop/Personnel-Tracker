0495e8079db86e8ef5f2b0cceaf72c0de0a4baff
Andre Diop <itsjustandre@gmail.com>
2021-02-15 18:40:48 -0500

    Created TableRow component

---
+++ src/components/TableRow/index.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+    render() {
+        return (
+            <>
+                  <tr>
+                    <th>
+                      <img
+                        src='https://images.unsplash.com/photo-1598316560445-dfeb1391d457?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1652&q=80'
+                        width='75'
+                        height='75'
+                      ></img>
+                    </th>
+                    <td>
+                      <p>Obie Fernandez</p>
+                    </td>
+                    <td>(919)354-3510</td>
+                    <td>ObFern@Gmail.com</td>
+                    <td>1/13/82</td>
+                  </tr> 
+            </>
+        );
+    }
+}
+
+export default index;
