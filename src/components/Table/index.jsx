cc7ba7cbac849fdb302bba1176c62df295a1fb0d
Andre Diop <itsjustandre@gmail.com>
2021-02-15 17:17:34 -0500

    Correct display of table
    - Moving the table into a centered colomn seeemed to do the trick with displaying it in the page.

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -4,42 +4,48 @@ class index extends Component {
   render () {
     return (
       <div>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>
-                <abbr title='Picture'>Picture</abbr>
-              </th>
-              <th>Name</th>
-              <th>
-                <abbr title='Phone'>Telephone</abbr>
-              </th>
-              <th>
-                <abbr title='Email'>Email</abbr>
-              </th>
-              <th>
-                <abbr title='Dob'>DOB</abbr>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th>
-                <img
-                  src='https://images.unsplash.com/photo-1528679382855-71c1ab151b12?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fGhlYWRzaG90fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
-                  width='75'
-                  height='75'
-                ></img>
-              </th>
-              <td>
-                <p>Lisa Bean</p>
-              </td>
-              <td>(404)-867-5309</td>
-              <td>LisaBean@Aol.com</td>
-              <td>10/14/92</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className='container'>
+          <div className='columns is-centered is-vcentered is-mobile'>
+            <div className='column is-narrow has-text-centered'>
+              <table className='table'>
+                <thead>
+                  <tr>
+                    <th>
+                      <abbr title='Picture'>Picture</abbr>
+                    </th>
+                    <th>Name</th>
+                    <th>
+                      <abbr title='Phone'>Telephone</abbr>
+                    </th>
+                    <th>
+                      <abbr title='Email'>Email</abbr>
+                    </th>
+                    <th>
+                      <abbr title='Dob'>DOB</abbr>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>
+                      <img
+                        src='https://images.unsplash.com/photo-1528679382855-71c1ab151b12?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fGhlYWRzaG90fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+                        width='75'
+                        height='75'
+                      ></img>
+                    </th>
+                    <td>
+                      <p>Lisa Bean</p>
+                    </td>
+                    <td>(404)-867-5309</td>
+                    <td>LisaBean@Aol.com</td>
+                    <td>10/14/92</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
