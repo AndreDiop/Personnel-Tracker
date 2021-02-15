54599a0bba7321e25c52fb9d5d89d8b9f812cc1f
Andre Diop <itsjustandre@gmail.com>
2021-02-15 18:39:03 -0500

    Format refactor

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,15 +3,16 @@ import React, { Component } from 'react'
 class index extends Component {
   render () {
     return (
-      <div>
-        <section className='hero is-link'>
-          <div className='hero-body has-text-centered'>
-            <p className='title is-size-1'>Personnel Tracker</p>
-            <p className='subtitle'>Click on controls to filter by heading or use the search to narrow by results.</p>
-          </div>
-        </section>
-      </div>
-    ) 
+      <section className='hero is-link'>
+        <div className='hero-body has-text-centered'>
+          <p className='title is-size-1'>Personnel Tracker</p>
+          <p className='subtitle'>
+            Click on controls to filter by heading or use the search to narrow
+            by results.
+          </p>
+        </div>
+      </section>
+    )
   }
 }
 
