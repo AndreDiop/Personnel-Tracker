2c532b2aa931bef1c39e9f9450360afec146c872
Andre Diop <itsjustandre@gmail.com>
2021-02-16 16:05:14 -0500

    Removed react-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import Header from "./components/Header";
 import SearchBar from "./components/SearchBar";
 import TableRow from "./components/TableRow";
